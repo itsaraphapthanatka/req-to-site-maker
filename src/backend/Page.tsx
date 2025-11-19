d8d0170ea50aed450ae742b86d713b82fcd56b3e
itsaraphap <itsaraphap.top@gmail.com>
2025-11-20 04:14:47 +0700

    8yt76

--- src/backend/Page.tsx
+++ src/backend/Page.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { User, Lock } from 'lucide-react';
+import { Image } from 'antd';
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -37,7 +38,7 @@ const LoginPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100 p-4">
       <div className="w-full max-w-md bg-white rounded-lg shadow-xl p-8">
-        <img src="../src/assets/logo.png" alt="Logo" />
+        <Image src="../src/assets/logo.png"/>
         <form className="space-y-4" onSubmit={handleSubmit}>
           <div>
             <label>Email</label>
