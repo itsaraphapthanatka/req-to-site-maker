11204f8d895f858aa9644c14c046d31d595c0ebf
itsaraphap <itsaraphap.top@gmail.com>
2025-12-03 11:06:01 +0700

    sdfsdf

--- src/components/[id]/gallory/detail.tsx
+++ src/components/[id]/gallory/detail.tsx
@@ -206,9 +206,9 @@ const GalloryDetailID = () => {
             <h2 className="text-2xl font-bold mb-4">{title}</h2>
             <div
               className="prose max-w-none"
-            // dangerouslySetInnerHTML={{ __html: descriptionHtml }}
+              dangerouslySetInnerHTML={{ __html: descriptionHtml }}
             >
-              {descriptionHtml}
+              {/* {descriptionHtml} */}
             </div>
           </div>
         )}
